9074a073f5563e033a3680cf29ea755f20d15730
Robin Goetz <tug29225@temple.edu>
2022-01-04 18:11:14 +0100

    added icons and language

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,6 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <html lang="de" />
         <title>Augsburger Stadtwichtel</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="/rainbow.png" />
@@ -29,6 +28,24 @@ const Home: NextPage = () => {
         />
         <meta property="og:url" content={`https://stadtwichtel-augsburg.de`} />
         <meta property="og:type" content="website" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
       </Head>
       <Header />
       <Element name={HeaderLinks.home.path}>
